e7bc41b9492c48f6c674dacfd3448f455969e331
Mbata Precious <mbataprecious9@gmail.com>
2026-09-03 18:09:57 +0100

    update tawkto

--- src/components/TawkMessenger.tsx
+++ src/components/TawkMessenger.tsx
@@ -8,7 +8,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/69c5135be85b2f1c34e634ec/1jkkt77fj';
+s1.src='https://embed.tawk.to/6a99a3c2ce478f34475b720c/1k1k2fc8g';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
